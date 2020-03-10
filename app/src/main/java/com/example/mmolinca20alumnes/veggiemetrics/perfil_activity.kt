6c58cf60468b2abee2a5f3471dd183a5d5133cd1
Clivi04 <Clivi04@hotmail.com>
2020-03-10 10:59:07 +0100

    layouts bàsics + barra inferior per navegar entre elles

--- app/src/main/java/com/example/mmolinca20alumnes/veggiemetrics/perfil_activity.kt
+++ app/src/main/java/com/example/mmolinca20alumnes/veggiemetrics/perfil_activity.kt
@@ -5,11 +5,11 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class perfil_activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_perfil)
     }
 
     fun home_action(view: View){
--- app/src/main/java/com/example/mmolinca20alumnes/veggiemetrics/receptes_activity.kt
+++ app/src/main/java/com/example/mmolinca20alumnes/veggiemetrics/receptes_activity.kt
@@ -5,11 +5,11 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class receptes_activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_receptes)
     }
 
     fun home_action(view: View){
