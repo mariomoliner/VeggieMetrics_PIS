420195ce10efa4cab485a0f0a97dcd0dbe9c0e8b
Christian <cguerrero97@gmail.com>
2020-03-30 15:08:07 +0200

    Implementació del test setmanal

--- app/src/main/java/com/example/mmolinca20alumnes/veggiemetrics/profileFragment.kt
+++ app/src/main/java/com/example/mmolinca20alumnes/veggiemetrics/profileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.mmolinca20alumnes.veggiemetrics
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -8,12 +9,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.fragment_profile.*
 import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class profileFragment : Fragment() {
+
+    val TEST_REQUEST = 1
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
@@ -23,11 +27,33 @@ class profileFragment : Fragment() {
         //Botó que porta al test setmanal:
         view.test_button.setOnClickListener {
             val intent = Intent(activity, testSetmanal::class.java)
-            startActivity(intent)
+            /*intent.putExtra("weight", weight.text.toString().toDouble())
+            intent.putExtra("sex", sex.selectedItem.toString())
+            intent.putExtra("diet", diet.selectedItem.toString())
+            intent.putExtra("age", height.text.toString().toInt())   //Canviar més endavant
+            intent.putExtra("pregnant", diet.selectedItem.toString())   //Canviar més endavant
+*/
+            intent.putExtra("weight", 65.0)
+            intent.putExtra("sex", "Home")
+            intent.putExtra("diet", "Flexitarià")
+            intent.putExtra("age", 22)
+            intent.putExtra("pregnant","No")
+            startActivityForResult(intent, TEST_REQUEST)
         }
 
         // Inflate the layout for this fragment
         return view
+    }
+
+    //Funció que obté els resultats del test
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode==TEST_REQUEST) {
+            if (resultCode== Activity.RESULT_OK) {
+                val results = data!!.getStringArrayListExtra("Resultats")
+            }
+        }
     }
 
 }
