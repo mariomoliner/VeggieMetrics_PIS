3de0a2e25ee085c4110300bc00a953b0df1258fb
Christian <cguerrero97@gmail.com>
2020-04-12 09:59:01 +0200

    Perfil acabat

--- app/src/main/java/com/example/mmolinca20alumnes/veggiemetrics/profileFragment.kt
+++ app/src/main/java/com/example/mmolinca20alumnes/veggiemetrics/profileFragment.kt
@@ -1,13 +1,16 @@
 package com.example.mmolinca20alumnes.veggiemetrics
 
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.renderscript.Sampler
 import android.view.*
-import android.widget.ArrayAdapter
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.google.android.gms.common.internal.Objects.equal
@@ -18,7 +21,10 @@ import kotlinx.android.synthetic.main.fragment_profile.*
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.fragment_profile.*
 import kotlinx.android.synthetic.main.fragment_profile.view.*
 import java.util.*
@@ -50,6 +56,20 @@ class profileFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
+    //Funció que modifica les barres del perfil
+    fun testResults(barra: ProgressBar, resultat: String) {
+        if (resultat.equals("Ok")) {
+            barra.getProgressDrawable().setColorFilter(Color.rgb(255,255,0), android.graphics.PorterDuff.Mode.MULTIPLY)
+            barra.progress = 100
+        } else if (resultat.equals("Not good")) {
+            barra.getProgressDrawable().setColorFilter(Color.rgb(255,255,0), android.graphics.PorterDuff.Mode.MULTIPLY)
+            barra.progress = 75
+        } else if (resultat.equals("Bad")) {
+            barra.getProgressDrawable().setColorFilter(Color.rgb(255,255,0), android.graphics.PorterDuff.Mode.MULTIPLY)
+            barra.progress = 50
+        }
+    }
+
     //Funció que obté els resultats del test
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -57,15 +77,14 @@ class profileFragment : Fragment() {
         if (requestCode==TEST_REQUEST) {
             if (resultCode== Activity.RESULT_OK) {
                 val results = data!!.getStringArrayListExtra("Resultats")
-                proteines.text = results[0]
-                ferro.text = results[1]
-                omega.text = results[2]
-                calci.text = results[3]
+                testResults(proteines, results[0])
+                testResults(ferro, results[1])
+                testResults(omega, results[2])
+                testResults(calci, results[3])
                 comentaris.text = results[4]
             }
         }
 
-        //Conflicte
         if(requestCode == PICK_PHOTO && resultCode ==  Activity.RESULT_OK && data != null){
             imagepicked = data.data
             //profilePic.setImageURI(imagepicked)
@@ -82,10 +101,8 @@ class profileFragment : Fragment() {
         list_pregnant = resources.getStringArray(R.array.Pregnant).toList()
         list_dietas = resources.getStringArray(R.array.Dietes).toList()
         list_sex = resources.getStringArray(R.array.Sex).toList()
-        list_allergy = resources.getStringArray(R.array.Allergy).toList()
         sex.adapter = ArrayAdapter(activity,android.R.layout.simple_spinner_item,list_sex)
         diet.adapter = ArrayAdapter(activity,android.R.layout.simple_spinner_item,list_dietas)
-        allergy.adapter = ArrayAdapter(activity,android.R.layout.simple_spinner_item,list_allergy)
         pregnant.adapter = ArrayAdapter(activity,android.R.layout.simple_spinner_item,list_pregnant)
 
 
@@ -102,12 +119,43 @@ class profileFragment : Fragment() {
         listener_canvis()
         photo_listener()
         listener_botoTest()
+        listener_sex()
     }
 
     fun setUser(userloged : FirebaseAuth){
         auth = userloged
     }
 
+    //Càlcul del IMC
+    fun imcCalcul() {
+        val h = height.text.toString().toDouble()
+        val w = weight.text.toString().toDouble()
+
+        if (h*w == 0.0) {
+            imc_text.setText("Sense dades")
+            imc_number.setText("")
+        }
+        else {
+            val imcResult = w/(h*h)
+            imc_number.setText(String.format("%.2f", imcResult).toString())
+            if (imcResult < 18.5)
+                imc_text.setText("Pes insuficient")
+            else if (imcResult < 25)
+                imc_text.setText("Pes normal")
+            else if (imcResult < 27)
+                imc_text.setText("Sobrepès grau I")
+            else if (imcResult < 30)
+                imc_text.setText("Sobrepès grau II")
+            else if (imcResult < 35)
+                imc_text.setText("Obesitat grau I")
+            else if (imcResult < 40)
+                imc_text.setText("Obesitat grau II")
+            else if (imcResult < 50)
+                imc_text.setText("Obesitat grau III")
+            else
+                imc_text.setText("Obesitat grau IV")
+        }
+    }
 
     fun setGUIuser(){
         progress_bar.visibility = View.VISIBLE
@@ -133,7 +181,7 @@ class profileFragment : Fragment() {
         dataref.addListenerForSingleValueEvent(object : ValueEventListener{
             override fun onCancelled(p0: DatabaseError) {
             }
-            //carreguem les dades del db: alçada, pes, dieta i sexe_
+            //carreguem les dades del db: alçada, pes, edat, dieta, sexe, embaràs, al·lèrgies i l'IMC_
             override fun onDataChange(p0: DataSnapshot) {
                 /* height.setText(p0.child("height").getValue().toString())
                 weight.setText(p0.child("weight").getValue().toString()) */
@@ -147,6 +195,9 @@ class profileFragment : Fragment() {
                     weight.setText("0.0")
                 else
                     weight.setText(p0.child("weight").getValue().toString())
+
+                imcCalcul()
+
 
                 if (p0.child("age").getValue().toString().equals("null"))
                     age.setText("0")
@@ -171,6 +222,46 @@ class profileFragment : Fragment() {
                         list_pregnant as ArrayList<String>
                     ))
                 }
+                when(p0.child("Allergy1").getValue()){
+                    null -> allergy1.isChecked = false
+                    else -> allergy1.isChecked = p0.child("Allergy1").getValue().toString().toBoolean()
+                }
+                when(p0.child("Allergy2").getValue()){
+                    null -> allergy2.isChecked = false
+                    else -> allergy2.isChecked = p0.child("Allergy2").getValue().toString().toBoolean()
+                }
+                when(p0.child("Allergy3").getValue()){
+                    null -> allergy3.isChecked = false
+                    else -> allergy3.isChecked = p0.child("Allergy3").getValue().toString().toBoolean()
+                }
+                when(p0.child("Allergy4").getValue()){
+                    null -> allergy4.isChecked = false
+                    else -> allergy4.isChecked = p0.child("Allergy4").getValue().toString().toBoolean()
+                }
+                when(p0.child("Allergy5").getValue()){
+                    null -> allergy5.isChecked = false
+                    else -> allergy5.isChecked = p0.child("Allergy5").getValue().toString().toBoolean()
+                }
+                when(p0.child("Allergy6").getValue()){
+                    null -> allergy6.isChecked = false
+                    else -> allergy6.isChecked = p0.child("Allergy6").getValue().toString().toBoolean()
+                }
+                when(p0.child("Allergy7").getValue()){
+                    null -> allergy7.isChecked = false
+                    else -> allergy7.isChecked = p0.child("Allergy7").getValue().toString().toBoolean()
+                }
+                when(p0.child("Allergy8").getValue()){
+                    null -> allergy8.isChecked = false
+                    else -> allergy8.isChecked = p0.child("Allergy8").getValue().toString().toBoolean()
+                }
+                when(p0.child("Allergy9").getValue()){
+                    null -> allergy9.isChecked = false
+                    else -> allergy9.isChecked = p0.child("Allergy9").getValue().toString().toBoolean()
+                }
+                when(p0.child("Allergy10").getValue()){
+                    null -> allergy10.isChecked = false
+                    else -> allergy10.isChecked = p0.child("Allergy10").getValue().toString().toBoolean()
+                }
                 progress_bar.visibility = View.INVISIBLE
                 activity!!.window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
             }
@@ -178,6 +269,8 @@ class profileFragment : Fragment() {
         })
 
     }
+
+
     //Listener del botó pel test setmanal:
     private fun listener_botoTest(){
         test_button.setOnClickListener{
@@ -186,8 +279,31 @@ class profileFragment : Fragment() {
             intent.putExtra("sex", sex.selectedItem.toString())
             intent.putExtra("diet", diet.selectedItem.toString())
             intent.putExtra("age", age.text.toString().toInt())
-            intent.putExtra("pregnant", diet.selectedItem.toString())
+            intent.putExtra("pregnant", pregnant.selectedItem.toString())
             startActivityForResult(intent, TEST_REQUEST)
+        }
+    }
+
+    //Listener del Spinner sexe:
+    private fun listener_sex() {
+        sex.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                if (sex.selectedItemPosition == 0) {
+                    pregnant.isEnabled = false
+                    pregnant.setSelection(1)
+                } else if (sex.selectedItemPosition == 1)
+                    pregnant.isEnabled = true
+
+            }
         }
     }
 
@@ -216,6 +332,19 @@ class profileFragment : Fragment() {
                     childUpdates["Gender"] = sex.selectedItem.toString()
                     childUpdates["Diet"] = diet.selectedItem.toString()
                     childUpdates["Pregnant"] = pregnant.selectedItem.toString()
+                    childUpdates["Allergy1"] = allergy1.isChecked
+                    childUpdates["Allergy2"] = allergy2.isChecked
+                    childUpdates["Allergy3"] = allergy3.isChecked
+                    childUpdates["Allergy4"] = allergy4.isChecked
+                    childUpdates["Allergy5"] = allergy5.isChecked
+                    childUpdates["Allergy6"] = allergy6.isChecked
+                    childUpdates["Allergy7"] = allergy7.isChecked
+                    childUpdates["Allergy8"] = allergy8.isChecked
+                    childUpdates["Allergy9"] = allergy9.isChecked
+                    childUpdates["Allergy10"] = allergy10.isChecked
+
+                    imcCalcul()
+
                     //actualitzem la info de l'usuari actual:
                     database.child("users-data").child(auth.currentUser!!.uid).updateChildren(childUpdates)
 
